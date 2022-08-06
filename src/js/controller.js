9e717fa4fcd157dce47300366a2439cc8ce8aee9
michaeljohnbrown3 <michaeljohnbrown3@gmail.com>
2022-08-05 22:56:01 -0400

    added navbar functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,13 @@ import '../sass/main.scss';
 import * as model from './model';
 
 import { navTitle, navItem } from './views/navView';
-import { header, aboutUs, services, aboutUsHeight } from './views/View';
+import {
+  header,
+  aboutUs,
+  services,
+  aboutUsHeight,
+  tagline,
+} from './views/View';
 
 const displaceHidden = function (entries) {
   const [entry] = entries;
@@ -11,12 +17,53 @@ const displaceHidden = function (entries) {
   else navTitle.classList.add('hidden');
 };
 
+let activeSidebarLink = 'tagline-container';
+
+const removeActiveClassFromSidebar = function () {
+  const sideNavItems = document.querySelectorAll('.side-nav__item');
+  console.log(sideNavItems);
+  sideNavItems.forEach(item => {
+    if (item.className.includes('active')) {
+      item.classList.remove('side-nav__item--active');
+    }
+  });
+};
+
 const displaySection = function (entries) {
   const [entry] = entries;
+  console.log({ entries });
+  const entryTarget = entry.target.className;
   console.log(entry);
+  console.log({ entryTarget });
+  console.log({ activeSidebarLink });
 
-  if (!entry.isIntersecting) navItem.classList.remove('side-nav__item--active');
-  else navItem.classList.add('side-nav__item--active');
+  if (entry.isIntersecting) {
+    if (entryTarget === 'tagline-container') {
+      activeSidebarLink = 'tagline-container';
+      removeActiveClassFromSidebar();
+      return;
+    }
+
+    if (activeSidebarLink !== entryTarget) {
+      // remove any active link if it's selected
+      if (activeSidebarLink !== 'tagline-container') {
+        console.log('triggered');
+        const navActiveLink = activeSidebarLink.replace('section', 'side-nav');
+        const navActiveLinkEl = document.querySelector(`#${navActiveLink}`);
+        navActiveLinkEl.classList.remove('side-nav__item--active');
+      }
+      // Config which el is new active link
+      activeSidebarLink = entryTarget;
+
+      // Applying class to config active link
+      const navLink = entryTarget.replace('section', 'side-nav');
+      const navLinkEl = document.querySelector(`#${navLink}`);
+      navLinkEl.classList.add('side-nav__item--active');
+    }
+  }
+
+  // if (!entry.isIntersecting) navItem.classList.remove('side-nav__item--active');
+  // else navItem.classList.add('side-nav__item--active');
 };
 
 const headerObserver = new IntersectionObserver(displaceHidden, {
@@ -29,15 +76,16 @@ const sectionObserver = new IntersectionObserver(displaySection, {
   threshold: 0,
 });
 
-const sectionTwoObserver = new IntersectionObserver(displaySection, {
-  root: null,
-  threshold: 0,
-});
+// const sectionTwoObserver = new IntersectionObserver(displaySection, {
+//   root: null,
+//   threshold: 0,
+// });
 
 const init = function () {
   headerObserver.observe(header);
   sectionObserver.observe(aboutUs);
-  sectionTwoObserver.observe(services);
+  sectionObserver.observe(services);
+  sectionObserver.observe(tagline);
 };
 init();
 
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,6 +2,7 @@ export const pictureBorder = document.querySelector('.picture-border');
 export const header = document.querySelector('.header');
 export const aboutUs = document.querySelector('.section__aboutus');
 export const services = document.querySelector('.section__services');
+export const tagline = document.querySelector('.tagline-container');
 
 export const headerHeight = header.getBoundingClientRect().height;
 export const aboutUsHeight = aboutUs.getBoundingClientRect().height;
